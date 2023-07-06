e2fdcdfa43e6cc49f23ff5c2cfa940d68729d8df
renanhconstancio <renanhconstancio@gmail.com>
2023-07-06 19:59:32 -0300

    feat: add footer to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,31 +3,33 @@ import './globals.css'
 
 import { NextAuthProvider } from '@/providers/auth'
 import { Poppins } from 'next/font/google'
+import Footer from '@/components/Footer'
 
 const poppins = Poppins({
-  subsets: ['latin'],
-  variable: '--font-poppins',
-  weight: ['400'],
+	subsets: ['latin'],
+	variable: '--font-poppins',
+	weight: ['400'],
 })
 
 export const metadata = {
-  title: 'FSW Trips',
-  description: 'Sistema de viagens top',
+	title: 'FSW Trips',
+	description: 'Sistema de viagens top',
 }
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode
+	children: React.ReactNode
 }) {
-  return (
-    <html lang="pt-br" className={`${poppins.variable}`}>
-      <body>
-        <NextAuthProvider>
-          <Header />
-          {children}
-        </NextAuthProvider>
-      </body>
-    </html>
-  )
+	return (
+		<html lang="pt-br" className={`${poppins.variable}`}>
+			<body>
+				<NextAuthProvider>
+					<Header />
+					{children}
+					<Footer />
+				</NextAuthProvider>
+			</body>
+		</html>
+	)
 }
